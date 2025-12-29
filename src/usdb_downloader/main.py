50d8cd17cf12cc6cfe441786df7adb4f5b7db7b6
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2025-12-29 17:41:31 +0100

    Add Parser

--- src/usdb_downloader/main.py
+++ src/usdb_downloader/main.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello from usdb-downloader!")
+from usdb_downloader.parser import Parser
+
+
+def main() -> None:
+    parser = Parser()
+    for file in parser.iter_files():
+        parser.write_file(file)
 
 
 if __name__ == "__main__":
---
+++ src/usdb_downloader/parser.py
@@ -0,0 +1,83 @@
+import re
+import logging
+from dataclasses import field, dataclass
+from pathlib import Path
+from typing import Final, Generator
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class File:
+    name: str
+    video_id: str
+    headers: dict[str, str] = field(default_factory=dict[str, str])
+    lyrics: list[str] = field(default_factory=list[str])
+
+
+class Parser:
+    _ID_PATTERN: Final[re.Pattern[str]] = re.compile(r"(?:a=|v=)([A-Za-z0-9_-]{11})")
+    _INPUT_DIR: Final[Path] = Path("./songs/input")
+    _OUTPUT_DIR: Final[Path] = Path("./songs/output")
+
+    def iter_files(self) -> Generator[File, None, None]:
+        if not self._INPUT_DIR.exists():
+            logger.warning(f"Input directory does not exist: {self._INPUT_DIR}")
+            return
+
+        for path in self._INPUT_DIR.glob("*.txt"):
+            song = self._parse_file(path)
+            if song:
+                yield song
+
+    def write_file(self, file: File) -> None:
+        output_file = self._OUTPUT_DIR / f"{file.name}.txt"
+        output_file.parent.mkdir(parents=True, exist_ok=True)
+        with open(output_file, "w", encoding="utf-8") as f:
+            for key, value in file.headers.items():
+                f.write(f"#{key}:{value}\n")
+            f.writelines(f"{line}\n" for line in file.lyrics)
+
+        logger.info(f"Wrote file {output_file}")
+
+    def _parse_file(self, path: Path) -> File | None:
+        name = path.stem
+        video_id: str | None = None
+        headers: dict[str, str] = {}
+        lyrics: list[str] = []
+
+        with path.open("r", encoding="utf-8") as src:
+            for raw_line in src:
+                line = raw_line.strip()
+
+                if not line or line.startswith(("#MP3", "#COVER")):
+                    continue
+
+                match line:
+                    case l if l.startswith("#VIDEO"):
+                        video_id = self._extract_video_id(l)
+                    case s if s.startswith("#"):
+                        key, _, value = s[1:].partition(":")
+                        headers[key.strip()] = value.strip()
+                    case _:
+                        lyrics.append(line)
+
+        if video_id is None:
+            return None
+
+        headers["COVER"] = f"{name}.jpg"
+        headers["MP3"] = f"{name}.mp3"
+        headers["VIDEO"] = f"{name}.webm"
+        logger.info(f"Parsed file {name}")
+
+        return File(
+            name=name,
+            video_id=video_id,
+            headers=headers,
+            lyrics=lyrics,
+        )
+
+    @staticmethod
+    def _extract_video_id(line: str) -> str | None:
+        match = Parser._ID_PATTERN.search(line)
+        return match.group(1) if match else None
