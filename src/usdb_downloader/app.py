c972da07d74e442b442d3bc6b92326f8f84730d1
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2026-01-01 18:22:47 +0100

    Make properties private in App

--- src/usdb_downloader/app.py
+++ src/usdb_downloader/app.py
@@ -17,18 +17,18 @@ class App:
         output_dir: Path,
         console: Console,
     ) -> None:
-        self.input_dir = input_dir
-        self.output_dir = output_dir
-        self.console = console
-        self.parser = Parser(input_dir=input_dir, output_dir=output_dir)
-        self.youtube_downloader = YoutubeDownloader()
+        self._input_dir = input_dir
+        self._output_dir = output_dir
+        self._console = console
+        self._parser = Parser(input_dir=input_dir, output_dir=output_dir)
+        self._youtube_downloader = YoutubeDownloader()
 
     async def run(self) -> None:
         logger.info("Starting application")
 
-        files = list(self.parser.iter_files())
+        files = list(self._parser.iter_files())
 
-        self.console.print_song_count(len(files))
+        self._console.print_song_count(len(files))
         if not files:
             return
 
@@ -36,39 +36,39 @@ class App:
         failed = 0
 
         for idx, file in enumerate(files, 1):
-            self.console.print_song_start(idx=idx, total=len(files), name=file.name)
+            self._console.print_song_start(idx=idx, total=len(files), name=file.name)
 
             try:
-                self.parser.write_file(file)
-                self.console.print_song_step("Parsed song file")
+                self._parser.write_file(file)
+                self._console.print_song_step("Parsed song file")
 
                 video_id = file.video_id
-                output_path = self.output_dir / file.name / file.name
+                output_path = self._output_dir / file.name / file.name
 
-                with self.console.print_song_step_spinner(
+                with self._console.print_song_step_spinner(
                     f"Downloading audio and video (ID: {video_id})"
                 ):
                     await asyncio.gather(
-                        self.youtube_downloader.download_audio(
+                        self._youtube_downloader.download_audio(
                             video_id=video_id,
                             output_path=output_path,
                         ),
-                        self.youtube_downloader.download_video(
+                        self._youtube_downloader.download_video(
                             video_id=video_id,
                             output_path=output_path,
                         ),
                     )
 
-                self.console.print_song_step(
+                self._console.print_song_step(
                     f"Downloading audio and video (ID: {video_id})"
                 )
 
-                self.console.print_song_success()
+                self._console.print_song_success()
                 processed += 1
 
             except YoutubeDownloaderException:
-                self.console.print_song_error("Failed to download audio and video")
+                self._console.print_song_error("Failed to download audio and video")
                 failed += 1
 
-        self.console.print_summary(processed=processed, failed=failed)
+        self._console.print_summary(processed=processed, failed=failed)
         logger.info("Finished application")
